142c12ec383accf4ed43a3ce6f64d296fbbdc249
Ryan Harings <ryanharings@hotmail.com>
2016-07-23 12:43:05 -0600

    working site, before css magic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,29 +6,43 @@ $('document').ready(function() {
     method: 'GET',
     dataType: 'json'
   }).done(function(roleResponse) { //grab role values from API and assign to variables
-    var role1 = roleResponse[0];
-    var role2 = roleResponse[1];
-    var role3 = roleResponse[2];
-    var role4 = roleResponse[3];
+    var roles = {};
 
-    $('.roles').append( //DOM manipulation to add role options/value/class
-      '<option class="role1" value="' + role1.img + '">' + role1.title + '</option>',
-      '<option class="role2" value="' + role2.img + '">' + role2.title + '</option>',
-      '<option class="role3" value="' + role3.img + '">' + role3.title + '</option>',
-      '<option class="role4" value="' + role4.img + '">' + role4.title + '</option>'
-    );
+    for (var i =0; i < roleResponse.length; i++) {
+      roles[roleResponse[i].title] = roleResponse[i];
+      $('#roles').append( //DOM manipulation to add role options/value/class
+        '<option class="select-roles" value="' + roleResponse[i].title + '">' + roleResponse[i].title + '</option>'
+      );
+    }
 
-    $('.roles').on('click', childSelector, function(e) {
+    $('#roles').change(function(e) { //manipulates DOM img based on user role selection
       e.preventDefault();
-
+      var userRole = roles[$(this).val()].img;
+      $('#sprite').replaceWith('<img id="sprite" src="' + userRole +'">')
     })
-
-  })
-
+  });
 
   $('form').on('submit', function(e) {
     e.preventDefault();
-    console.log('working');
+
+    var userPOST = {
+      firstName: $('#first-name').val(),
+      lastName: $('#last-name').val(),
+      role: $('#roles').val()
+    }
+
+    $.ajax({
+      url: 'https://galvanize-student-apis.herokuapp.com/gpersonnel/users',
+      method: 'POST',
+      data: userPOST,
+      success: function(succ) {
+        $('#message > p').replaceWith('<p>' + succ.message + '</p>');
+      },
+      error: function(err) {
+        console.log(err);
+        $('#message> p').replaceWith('<p>' + err.responseJSON.message + '</p>')
+      }
+    })
   })
 
 });
